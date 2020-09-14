65ba481fcc7ead11dd6de2b7148fc892dea5e957
McKnightLight <themcknightrider@gmail.com>
2020-09-13 19:42:51 -0500

    initialize CLI project

---
+++ lib/got_api/version.rb
@@ -0,0 +1,3 @@
+module GotApi
+  VERSION = "0.1.0"
+end
---
+++ spec/got_api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GotApi do
+  it "has a version number" do
+    expect(GotApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
