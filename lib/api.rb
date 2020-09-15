3582255a8bb079397c47150fd735d1d8a7ce17c7
McKnightLight <themcknightrider@gmail.com>
2020-09-15 10:06:48 -0500

    tinkering...

--- lib/api.rb
+++ lib/api.rb
@@ -1,33 +1,67 @@
 class API
-  
-  BASE_URL = "https://anapioficeandfire.com/api/"
-
+  #BASE_URL = "https://anapioficeandfire.com/api/"
   def self.get_characters
-    response = RestClient.get(BASE_URL + "characters/")
-    data = JSON.parse(response)
-
-    data["results"].each do |characters_data|
-      name = characters_data["name"]
-
-    end
-    #binding.pry 
-    #resp = RestClient.get('https://anapioficeandfire.com/api/')
+    resp = RestClient.get('https://anapioficeandfire.com/api/characters/')
+    got_hash = JSON.parse(resp.body, symbolize_names:true)
     #binding.pry
-    #got_hash = JSON.parse(response.body, symbolize_names:true)
-    #got_arr = got_hash[:results]
-    #binding.pry
-    #got_arr.collect do | person |
+    got_hash.collect do | person |
       #binding.pry
-      #Characters.new(person)
-    #end
+      Characters.new(person)
+    end
   end
 
-  def self.get_character_details(characters)
-    response = RestClient.get(BASE_URL + "characters/")
-    char_hash = JSON.parse(response.body, symbolize_names:true)
-    characters.culture = char_hash[:culture]
-    characters.titles = char_hash[:titles]
-    characters.aliases = char_hash[:aliases]
-    characters.playedBy = char_hash[:playedBy]
+
+  def self.get_character_details(character)
+      resp = RestClient.get(character.url)
+      char_hash = JSON.parse(resp.body, symbolize_names:true)
+      character.height = char_hash[:height]
+      character.mass = char_hash[:mass]
+      character.hair_color = char_hash[:hair_color]
+      character.skin_color = char_hash[:skin_color]
+      character.eye_color = char_hash[:eye_color]
   end
+
+
 end
+
+
+# class API
+  
+#   BASE_URL = "https://anapioficeandfire.com/api/"
+
+#   def self.get_characters(next_page=nil)
+#     response = nil 
+#     if next_page
+#       response = RestClient.get(next_page)
+#     else
+#       response = RestClient.get(BASE_URL)
+#     end
+#     data = JSON.parse(response)
+
+#     got_hash = JSON.parse(response.body, symbolize_names:true)
+#     #binding.pry
+#     got_hash.collect do | person |
+#       #binding.pry
+#       Characters.new(person)
+#     end
+#   end
+
+#   def self.get_character_details(characters)
+#     response = RestClient.get(BASE_URL + "characters/")
+#     char_hash = JSON.parse(response.body, symbolize_names:true)
+#     characters.culture = char_hash[:culture]
+#     characters.titles = char_hash[:titles]
+#     characters.aliases = char_hash[:aliases]
+#     characters.playedBy = char_hash[:playedBy]
+#     characters.gender = char_hash[:gender]
+#     characters.born = char_hash[:born]
+#     characters.died = char_hash[:died]
+#     characters.mother = char_hash[:mother]
+#     characters.father = char_hash[:father]
+#     characters.spouse = char_hash[:spouse]
+#     characters.allegiances = char_hash[:allegiances]
+#     characters.books = char_hash[:books]
+#     characters.povBooks = char_hash[:povBooks]
+#     characters.tvSeries = char_hash[:tvSeries]
+#   end
+# end
--- lib/characters.rb
+++ lib/characters.rb
@@ -1,19 +1,35 @@
 class Characters
   
-  attr_accessor :name, :url, :culture, :titles, :aliases, :playedBy
+  attr_accessor :name, :url, :culture, :titles, :aliases, :playedBy, :gender, :born, :died, :father, :mother, :spouse, :allegiances, :books, :povBooks, :tvSeries
   
   @@all = []
 
-  def initialize(name:, url:, culture:, titles:, aliases:, playedBy:)
-    self.name = name
-    self.url = url
-    self.culture = culture
-    self.titles = titles 
-    self.aliases = aliases 
-    self.playedBy = playedBy
-  end 
+  def initialize(person)
+    self.name = person[:name]
+    self.url = person [:url]
+    @@all << self
+  end
+
+  # def initialize (name:, url:, culture:, titles:, aliases:, playedBy:, gender:, born:, died:, father:, mother:, spouse:, allegiances:, books:, povBooks:, tvSeries:)
+  #   self.name = name
+  #   self.url = url
+  #   self.culture = culture
+  #   self.titles = titles 
+  #   self.aliases = aliases 
+  #   self.playedBy = playedBy
+  #   self.gender = gender
+  #   self.born = born
+  #   self.died = died 
+  #   self.father = father
+  #   self.mother = mother
+  #   self.spouse = spouse 
+  #   self.allegiances = allegiances
+  #   self.books = books
+  #   self.povBooks = povBooks
+  #   self.tvSeries = tvSeries
+  # end 
 
   def self.all
-    @@all << self 
+    @@all
   end
 end
