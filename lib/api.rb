3c78b3760579c031a06d30bf2fdad864011d499b
McKnightLight <themcknightrider@gmail.com>
2020-09-15 10:46:50 -0500

    MVP (no not that MVP)

--- lib/api.rb
+++ lib/api.rb
@@ -14,11 +14,20 @@ class API
   def self.get_character_details(character)
       resp = RestClient.get(character.url)
       char_hash = JSON.parse(resp.body, symbolize_names:true)
-      character.height = char_hash[:height]
-      character.mass = char_hash[:mass]
-      character.hair_color = char_hash[:hair_color]
-      character.skin_color = char_hash[:skin_color]
-      character.eye_color = char_hash[:eye_color]
+      character.name = char_hash[:name]
+      character.culture = char_hash[:culture]
+      character.titles = char_hash[:titles]
+      character.aliases = char_hash[:aliases]
+      character.playedBy = char_hash[:playedBy]
+      character.gender = char_hash[:gender]
+      character.born = char_hash[:born]
+      character.died = char_hash[:died]
+      character.father = char_hash[:mother]
+      character.spouse = char_hash[:spouse]
+      character.allegiances = char_hash[:allegiances]
+      character.books = char_hash[:books]
+      character.povBooks = char_hash[:povBooks]
+      character.tvSeries = char_hash[:tvSeries]
   end
 
 
